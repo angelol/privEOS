cb8e0dfa4b4c568f761c551ead548d141b789e48
Angelo Laub <angelolaub@gmail.com>
2018-10-02 15:53:07 +0200

    basic contract that handles node registration

--- priveos/priveos.cpp
+++ priveos/priveos.cpp
@@ -1,16 +1,60 @@
-#include <eosiolib/eosio.hpp>
+#include "priveos.hpp"
 
 using namespace eosio;
 
 class priveos : public contract {
   public:
-      using contract::contract;
+    using contract::contract;
+    
+    priveos(account_name self) : contract(self), nodes(_self, _self) {}
 
-      [[eosio::action]]
-      void store(const account_name owner, const std::string file, const std::string data) {
-        require_auth(owner);
-        print( "Storing file ", file);
-      }
+    //@abi action
+    void store(const account_name owner, const std::string file, const std::string data) {
+      require_auth(owner);
+      // print( "Storing file ", file);
+    }
+    
+    //@abi action
+    void regnode(const account_name owner, const eosio::public_key node_key) {
+      eosio_assert(node_key != eosio::public_key(), "public key should not be the default value");
+      
+      require_auth(owner);
+      
+      auto node_idx = nodes.find(owner);
+      if(node_idx != nodes.end()) {
+        nodes.modify(node_idx, owner, [&](nodeinfo& info) {
+          info.node_key = node_key;
+        });
+      } else {
+        nodes.emplace(owner, [&](nodeinfo& info) {
+          info.owner = owner;
+          info.node_key = node_key;
+        });
+      }  
+    }
+    
+    //@abi action
+    void unregnode(const account_name owner) {
+      require_auth(owner);
+      const auto& node = nodes.get( owner, "owner not found." );
+      nodes.modify(node, 0, [&](nodeinfo& info) {
+        info.is_active = false;
+      });
+    }
+  
+  private:
+    // @abi table nodes i64
+    struct nodeinfo {
+      account_name  owner;
+      eosio::public_key    node_key;
+      bool          is_active = true;
+      
+      uint64_t primary_key()const { return owner; }
+      
+      EOSLIB_SERIALIZE(nodeinfo, (owner)(node_key)(is_active))
+    };
+    typedef multi_index<N(nodes), nodeinfo> nodes_table;
+    nodes_table nodes;
 };
 
-EOSIO_ABI( priveos, (store) )
+EOSIO_ABI( priveos, (store)(regnode)(unregnode) )
--- priveos/priveos.hpp
+++ priveos/priveos.hpp
@@ -3,5 +3,5 @@
  *  @copyright defined in eos/LICENSE.txt
  */
 #include <eosiolib/eosio.hpp>
-
+#include <eosiolib/public_key.hpp>
 
