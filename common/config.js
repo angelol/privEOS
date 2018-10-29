6b41f079882f3b43cdba5ce07d9f6b6183cb920a
Angelo Laub <angelolaub@gmail.com>
2018-10-29 22:31:33 +0100

    fix performance problem
    mongodb client seems to be really slow when using localhost instead of 127.0.0.1

--- common/config.js
+++ common/config.js
@@ -3,7 +3,7 @@
 export default {
   httpEndpoint: 'http://localhost:8888',
   chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f',
-  mongoUrl: 'mongodb://localhost:27017',
+  mongoUrl: 'mongodb://127.0.0.1:27017',
   dbName: 'EOS',
   contract: 'priveosrules',
   KMS_PORT: 3000,
