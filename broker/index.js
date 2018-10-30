3b202269818a5397bfa0b8372864f9360644ec21
Angelo Laub <angelolaub@gmail.com>
2018-10-30 12:10:34 +0100

    fix restify exception handling

--- broker/index.js
+++ broker/index.js
@@ -11,7 +11,7 @@ import config from '../common/config'
 import { get_node_urls, contract } from './helpers'
 import { get_store_trace } from '../common/mongo'
 
-const server = restify.createServer({handleUncaughtExceptions: true})
+const server = restify.createServer()
 server.use(restify.plugins.bodyParser())
 
  
@@ -63,21 +63,11 @@ server.post('/read/', function(req, res, next) {
   })
   .catch(err => {
     console.log("Error: ", err)
+    res.send("Generic Error")
   })
   next()
 })
 
-server.on('InternalServer', function(req, res, err, callback) {
-  // this will get fired first, as it's the most relevant listener
-	console.log('InternalServer')
-  return callback()
-})
-
-server.on('uncaughtException', function(req, res, route, err) {
-    // this event will be fired, with the error object from above:
-    // ReferenceError: x is not defined
-		console.log('uncaughtException ', err)
-})
 server.listen(config.BROKER_PORT, function() {
   console.log('Broker %s listening at %s', server.name, server.url)
 })
--- priveos_kms/index.js
+++ priveos_kms/index.js
@@ -14,7 +14,7 @@ if(process.argv[3]) {
 	config.nodeAccount = process.argv[3]
 }
 
-var server = restify.createServer({handleUncaughtExceptions: true})
+var server = restify.createServer()
 server.use(restify.plugins.bodyParser())
 
 
@@ -39,17 +39,6 @@ server.post('/read/', function(req, res, next) {
   next()
 })
 
-server.on('InternalServer', function(req, res, err, callback) {
-  // this will get fired first, as it's the most relevant listener
-	console.log('InternalServer')
-  return callback()
-})
-
-server.on('uncaughtException', function(req, res, route, err) {
-    // this event will be fired, with the error object from above:
-    // ReferenceError: x is not defined
-		console.log('uncaughtException ', err)
-})
 console.log("PORT: ", config.KMS_PORT)
 server.listen(config.KMS_PORT, function() {
   console.log('%s listening at %s', server.name, server.url)
