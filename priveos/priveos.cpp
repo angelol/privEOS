6ad969b6a8b65dd14c44c8feefec00cab51974bb
Angelo Laub <angelolaub@gmail.com>
2018-11-08 14:51:05 +0100

    update code to eosio.cdt 1.4

--- priveos/priveos.cpp
+++ priveos/priveos.cpp
@@ -1,129 +1,81 @@
 #include "priveos.hpp"
 
-using namespace eosio;
-
-class priveos : public contract {
-  public:
-    using contract::contract;
+// using namespace eosio;
+ACTION priveos::store(const name owner, const name contract, const std::string file, const std::string data) {
+  require_auth(owner);
+  // print( "Storing file ", file);
+}
     
-    priveos(account_name self) : contract(self), nodes(_self, _self), prices(_self, _self) {}
-
-    //@abi action
-    void store(const account_name owner, const account_name contract, const std::string file, const std::string data) {
-      require_auth(owner);
-      // print( "Storing file ", file);
-    }
+ACTION priveos::accessgrant(const name user, const name contract, const std::string file, const eosio::public_key public_key) {
+  require_auth(user);
+  require_recipient(contract);
+}
     
-    //@abi action
-    void accessgrant(const account_name user, const account_name contract, const std::string file, const eosio::public_key public_key) {
-      require_auth(user);
-      require_recipient(contract);
-    }
-    
-    //@abi action
-    void regnode(const account_name owner, const eosio::public_key node_key, const std::string url) {
-      eosio_assert(node_key != eosio::public_key(), "public key should not be the default value");
-      
-      require_auth(owner);
-      
-      auto node_idx = nodes.find(owner);
-      if(node_idx != nodes.end()) {
-        nodes.modify(node_idx, owner, [&](nodeinfo& info) {
-          info.node_key = node_key;
-          info.url = url;
-          info.is_active = true;
-        });
-      } else {
-        nodes.emplace(owner, [&](nodeinfo& info) {
-          info.owner = owner;
-          info.node_key = node_key;
-          info.url = url;
-        });
-      }  
-    }
-    
-    //@abi action
-    void unregnode(const account_name owner) {
-      require_auth(owner);
-      const auto& node = nodes.get(owner, "owner not found");
-      nodes.modify(node, 0, [&](nodeinfo& info) {
-        info.is_active = false;
-      });
-    }
-
-    
-    
-    void update_price(const account_name node, const asset price) {
-      pricefeed_table pricefeeds(_self, price.symbol);
-      std::vector<int64_t> vec;
-      for(const auto& pf : pricefeeds) {
-        vec.push_back(pf.price.amount);        
-      }
-      asset median_price = asset(median(vec), price.symbol);      
-      const auto& itr = prices.find(price.symbol);
-      if(itr != prices.end()) {
-        prices.modify(itr, node, [&](auto& p) {
-          p.money = median_price;
-        });
-      } else {
-        prices.emplace(node, [&](auto& p) {
-          p.money = median_price;
-        });
-      }
-    }
-    
-    //@abi action
-    void setprice(const account_name node, const asset price) {
-      nodes.get(node, "node not found.");
-      pricefeed_table pricefeeds(_self, price.symbol);
-      auto itr = pricefeeds.find(node);
-      
-      if(itr != pricefeeds.end()) {
-        pricefeeds.modify(itr, node, [&](pricefeed& pf) {
-          pf.price = price;
-        });
-      } else {
-        pricefeeds.emplace(node, [&](pricefeed& pf) {
-          pf.node = node;
-          pf.price = price;
-        });
-      }
-      update_price(node, price);
-    }
-    
-
+ACTION priveos::regnode(const name owner, const eosio::public_key node_key, const std::string url) {
+  eosio_assert(node_key != eosio::public_key(), "public key should not be the default value");
   
-  private:
-    // @abi table nodes i64
-    struct nodeinfo {
-      account_name        owner;
-      eosio::public_key   node_key;
-      std::string         url;
-      bool                is_active = true;
-      
-      uint64_t primary_key()const { return owner; }
-      
-      EOSLIB_SERIALIZE(nodeinfo, (owner)(node_key)(url)(is_active))
-    };
-    typedef multi_index<N(nodes), nodeinfo> nodes_table;
-    nodes_table nodes;
+  require_auth(owner);
+  
+  auto node_idx = nodes.find(owner.value);
+  if(node_idx != nodes.end()) {
+    nodes.modify(node_idx, owner, [&](nodeinfo& info) {
+      info.node_key = node_key;
+      info.url = url;
+      info.is_active = true;
+    });
+  } else {
+    nodes.emplace(owner, [&](nodeinfo& info) {
+      info.owner = owner;
+      info.node_key = node_key;
+      info.url = url;
+    });
+  }  
+}
     
-    // @abi table pricefeed i64
-    struct pricefeed {
-      account_name node;
-      asset price;
-      uint64_t primary_key() const { return node; }
-    };
-    typedef multi_index<N(pricefeed), pricefeed> pricefeed_table;
-    
-    // @abi table price i64
-    struct price {
-      asset money;
-      uint64_t primary_key() const { return money.symbol; }
-    };
-    typedef multi_index<N(price), price> price_table;
-    price_table prices;
-};
+ACTION priveos::unregnode(const name owner) {
+  require_auth(owner);
+  const auto& node = nodes.get(owner.value, "owner not found");
+  nodes.modify(node, eosio::same_payer, [&](nodeinfo& info) {
+    info.is_active = false;
+  });
+}
+
+ACTION priveos::setprice(const name node, const asset price) {
+  nodes.get(node.value, "node not found.");
+  pricefeed_table pricefeeds(_self, price.symbol.code().raw());
+  auto itr = pricefeeds.find(node.value);
+  
+  if(itr != pricefeeds.end()) {
+    pricefeeds.modify(itr, node, [&](pricefeed& pf) {
+      pf.price = price;
+    });
+  } else {
+    pricefeeds.emplace(node, [&](pricefeed& pf) {
+      pf.node = node;
+      pf.price = price;
+    });
+  }
+  update_price(node, price);
+}
+
+void priveos::update_price(const name node, const asset price) {
+  pricefeed_table pricefeeds(_self, price.symbol.code().raw());
+  std::vector<int64_t> vec;
+  for(const auto& pf : pricefeeds) {
+    vec.push_back(pf.price.amount);        
+  }
+  asset median_price = asset{median(vec), price.symbol};      
+  const auto& itr = prices.find(price.symbol.code().raw());
+  if(itr != prices.end()) {
+    prices.modify(itr, node, [&](auto& p) {
+      p.money = median_price;
+    });
+  } else {
+    prices.emplace(node, [&](auto& p) {
+      p.money = median_price;
+    });
+  }
+}
 
 
-EOSIO_ABI( priveos, (store)(accessgrant)(regnode)(unregnode)(setprice) )
+EOSIO_DISPATCH( priveos, (store)(accessgrant)(regnode)(unregnode)(setprice) )
--- priveos/priveos.hpp
+++ priveos/priveos.hpp
@@ -5,18 +5,93 @@
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/public_key.hpp>
 #include <eosiolib/asset.hpp>
+#include <eosiolib/symbol.hpp>
 
-int64_t median(std::vector<int64_t>& v) {
-  size_t s = v.size();
-  if(s == 0) {
-    return 0;
-  } else if(s == 1) {
-    return v[0];
-  }    
-  std::sort(v.begin(), v.end());
-  if(s % 2 == 0) {
-    return (v[s/2-1] + v[s/2])/2;
-  } else {
-    return v[s/2];
-  }
-}
+using namespace eosio;
+
+CONTRACT priveos : public eosio::contract {
+  using contract::contract;
+  public:
+    priveos(name self,name code, datastream<const char*> ds) : eosio::contract(self,code,ds), nodes(_self, _self.value), prices(_self, _self.value){}
+    
+    TABLE nodeinfo {
+      name        owner;
+      eosio::public_key   node_key;
+      std::string         url;
+      bool                is_active = true;
+      
+      uint64_t primary_key()const { return owner.value; }      
+    };
+    
+    TABLE pricefeed {
+      name node;
+      asset price;
+      uint64_t primary_key() const { return node.value; }
+    };
+    
+    TABLE price {
+      asset money;
+      uint64_t primary_key() const { return money.symbol.code().raw(); }
+    };
+    
+    
+    typedef multi_index<"nodes"_n, nodeinfo> nodes_table;
+    typedef multi_index<"pricefeed"_n, pricefeed> pricefeed_table;
+    typedef multi_index<"price"_n, price> price_table;
+    
+    nodes_table nodes;
+    price_table prices;
+    
+    
+    ACTION store(
+      const name owner, 
+      const name contract, 
+      const std::string file, 
+      const std::string data
+    );
+    
+    ACTION accessgrant(
+      const name user, 
+      const name contract, 
+      const std::string file, 
+      const eosio::public_key public_key
+    );
+    
+    ACTION regnode(
+      const name owner, 
+      const eosio::public_key node_key, 
+      const std::string url
+    );
+    
+    ACTION unregnode(
+      const name owner
+    );
+    
+    ACTION setprice(
+      const name node,
+      const asset price
+    );
+    
+    
+    
+  private:
+    void update_price(
+      const name node, 
+      const asset price
+    );
+    
+    int64_t median(std::vector<int64_t>& v) {
+      size_t s = v.size();
+      if(s == 0) {
+        return 0;
+      } else if(s == 1) {
+        return v[0];
+      }    
+      std::sort(v.begin(), v.end());
+      if(s % 2 == 0) {
+        return (v[s/2-1] + v[s/2])/2;
+      } else {
+        return v[s/2];
+      }
+    }    
+};
