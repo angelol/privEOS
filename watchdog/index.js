65e7a7bc658e668e513bc61870d993293ce7565f
fabifrank <info@fabifrank.com>
2019-03-02 16:14:14 +0100

    watchdog running through; adding config to chainConnectors

--- watchdog/index.js
+++ watchdog/index.js
@@ -30,11 +30,17 @@ config.chains.forEach((chainConfig, index) => {
 })
 
 
-const chainConnectors = config.chains.map(chainConfig => Eos({
-  httpEndpoint: chainConfig.httpEndpoint, 
-  chainId: chainConfig.chainId,
-  keyProvider: [chainConfig.watchdogPermission.key],
-}))
+const chainConnectors = config.chains.map(chainConfig => {
+  console.log('chainConfig', chainConfig, chainConfig.watchdogPermission.key)
+  return {
+    eos: Eos({
+      httpEndpoint: chainConfig.httpEndpoint, 
+      chainId: chainConfig.chainId,
+      keyProvider: [chainConfig.watchdogPermission.key],
+    }),
+    config: chainConfig
+  }
+})
 
 
 if(process.argv[2]) {
@@ -50,18 +56,22 @@ const server = restify.createServer()
 let status = 'ok'
 
 server.get('/watchdog/status/', async function(req, res, next) {
-	try { 
-    const err = await check_permissions()
-    if(err) {
+  try {
+    let errors = []
+    for (let i = 0; i < chainConnectors.length; i++) {
+      let chain = chainConnectors[i]
+      const err = await check_permissions(chain)
+      if (err) errors.push(err)
+    }
+    if(errors.length > 0) {
       res.send({
-  	    status: err,
-  		})
+        status: errors,
+      })
     } else {
       res.send({
   	    status: status,
   		})
     }
-		
 	} catch(err) {
 		log.error("Error: ", err)
 		res.send(500, "Generic Error")
@@ -83,8 +93,9 @@ async function main() {
   let checkedNodes = [] // to avoid multiple check calls to the same node due to different chain ids, we store them here
 
   for (let i = 0; i < chainConnectors.length; i++) {
-    let eos = chainConnectors[i]
-    const watchdog_should_run = await should_watchdog_run(eos)
+    let chain = chainConnectors[i]
+    log.debug(`Run watchdog for ${chain.config.httpEndpoint}, ${chain.config.chainId}`)
+    const watchdog_should_run = await should_watchdog_run(chain)
     if(!watchdog_should_run) {
       log.info("Contract version is incompatible, skipping this round.")
       setTimeout(main, 10000)
@@ -92,10 +103,10 @@ async function main() {
     }
     
     // 1. get nodes
-    const nodes = await get_nodes(eos)
+    const nodes = await get_nodes(chain)
     log.debug('nodes', nodes)
-    approvals = await get_approvals(eos)
-    disapprovals = await get_disapprovals(eos)
+    approvals = await get_approvals(chain)
+    disapprovals = await get_disapprovals(chain)
     log.debug(`disapprovals: ${JSON.stringify(disapprovals)}`)
     // 2. check node status
     //    if okay and not active => approve
@@ -103,7 +114,7 @@ async function main() {
     //    with 30 nodes, 1 round takes 60 seconds
     for(const node of nodes) {
       try {
-        await handle_node(node, eos)
+        await handle_node(node, chain)
         status = 'ok'
       } catch(e) {
         log.error(e)
@@ -117,32 +128,32 @@ async function main() {
   setTimeout(main, 0)
 }
 
-async function handle_node(node, eos) {
+async function handle_node(node, chain) {
   const node_is_okay = await is_node_okay(node)
   log.debug(`Node ${node.owner} is ${node_is_okay}`)
   if(node_is_okay && !node.is_active) {
     log.info(`Node ${node.owner} is okay, approving`)
-    return approve(node, eos)
+    return approve(node, chain)
   }
   if(!node_is_okay && node.is_active) {
     log.info(`Node ${node.owner} is down, disapproving`)
-    return disapprove(node, eos)
+    return disapprove(node, chain)
   }
 }
 
-async function approve(node, eos) {
+async function approve(node, chain) {
   if(needs_my_approval(node)) {
     log.debug(`Node ${node.owner} needs my approval`)
-    return execute_transaction(node, 'peerappr', eos)
+    return execute_transaction(node, 'peerappr', chain)
   } else {
     log.debug(`Node ${node.owner} already approved by me`)
   }
 }
 
-async function disapprove(node, eos) {
+async function disapprove(node, chain) {
   if(needs_my_disapproval(node)) {
     log.debug(`Node ${node.owner} needs my disapproval`)
-    return execute_transaction(node, 'peerdisappr', eos)
+    return execute_transaction(node, 'peerdisappr', chain)
   } else {
     log.debug(`Node ${node.owner} already disapproved by me`)
   }
@@ -168,8 +179,8 @@ function needs_my_disapproval(node) {
   return !disapproval.includes(config.nodeAccount)  
 }
 
-async function get_approvals(eos) {
-  const res = await eos.getTableRows({json:true, scope: config.contract, code: config.contract,  table: 'peerapproval', limit:100})
+async function get_approvals(chain) {
+  const res = await chain.eos.getTableRows({json:true, scope: chain.config.contract, code: chain.config.contract, table: 'peerapproval', limit:100})
   return res.rows.reduce((a, b) => {
     if(!is_expired(b)) {
       a[b.node] = b.approved_by
@@ -178,8 +189,8 @@ async function get_approvals(eos) {
   }, {})
 }
 
-async function get_disapprovals(eos) {
-  const res = await eos.getTableRows({json:true, scope: config.contract, code: config.contract,  table: 'peerdisappr', limit:100})
+async function get_disapprovals(chain) {
+  const res = await chain.eos.getTableRows({json:true, scope: chain.config.contract, code: chain.config.contract, table: 'peerdisappr', limit:100})
   log.debug(`get_disapprovals: ${JSON.stringify(res.rows,null, 2)}`)
   return res.rows.reduce((a, b) => {
     if(!is_expired(b)) {
@@ -192,16 +203,17 @@ async function get_disapprovals(eos) {
 function is_expired(approval) {
   return ((new Date()).getTime()/1000 - approval.created_at) > 5*60 
 }
-async function execute_transaction(node, action_name, eos) {
+
+async function execute_transaction(node, action_name, chain) {
   const actions = [{
-    account: config.contract,
+    account: chain.config.contract,
     name: action_name,
     authorization: [{
-      actor: config.nodeAccount,
-      permission: config.watchdogPermission.permission,
+      actor: chain.config.nodeAccount,
+      permission: chain.config.watchdogPermission.permission,
     }],
     data: {
-      sender: config.nodeAccount,
+      sender: chain.config.nodeAccount,
       owner: node.owner,
     }
   }]
@@ -230,25 +242,25 @@ async function is_node_okay(node) {
   }
   return okay
 }
-async function get_nodes(eos) {
-  const res = await eos.getTableRows({json:true, scope: config.contract, code: config.contract,  table: 'nodes', limit:100})
-  return res.rows.filter(x => x.owner != config.nodeAccount)
+async function get_nodes(chain) {
+  const res = await chain.eos.getTableRows({json:true, scope: chain.config.contract, code: chain.config.contract, table: 'nodes', limit:100})
+  return res.rows.filter(x => x.owner != chain.config.nodeAccount)
 }
 
-async function should_watchdog_run(eos) {
-  const res = await eos.getAbi({account_name: config.contract})
+async function should_watchdog_run(chain) {
+  const res = await chain.eos.getAbi({account_name: chain.config.contract})
   const table_names = res.abi.tables.map(x => x.name)
   // console.log(JSON.stringify(table_names, null, 2))
   return table_names.includes('peerapproval')
 }
 
-async function check_permissions(eos) {
-  const res = await eos.getAccount(config.nodeAccount)
+async function check_permissions(chain) {
+  const res = await chain.eos.getAccount(chain.config.nodeAccount)
   // console.log("res: ", res)
-  const watchdog_perm = res.permissions.filter(x => x.perm_name == config.watchdogPermission.permission)[0]
+  const watchdog_perm = res.permissions.filter(x => x.perm_name == chain.config.watchdogPermission.permission)[0]
   
   if(!watchdog_perm) {
-    return `No permission found with name ${config.watchdogPermission.permission}`
+    return `No permission found with name ${chain.config.watchdogPermission.permission}`
   }
   // console.log("watchdog_perm.required_auth: ", JSON.stringify(watchdog_perm.required_auth, null, 2))
   const auth = watchdog_perm.required_auth
@@ -257,7 +269,7 @@ async function check_permissions(eos) {
     return "Threshold should be 1"
   }
   
-  const configured_public_key = eosjs_ecc.privateToPublic(config.watchdogPermission.key)
+  const configured_public_key = eosjs_ecc.privateToPublic(chain.config.watchdogPermission.key)
   
   if(auth.keys[0].key != configured_public_key) {
     return `Key is ${auth.keys[0].key} but configured key is ${configured_public_key}`
