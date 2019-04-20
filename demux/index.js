d35c6465f29c9c71094613a456bb72cda9970bbb
Angelo Laub <angelolaub@gmail.com>
2019-04-20 21:30:02 +0200

    remove some more redundant requires

--- demux/index.js
+++ demux/index.js
@@ -1,8 +1,6 @@
 'use strict'
 const config = require('../common/config')
 const log = require('../common/log')
-const { fork } = require('child_process')
-const chains = require('../common/chains')
 const Demux = require('./demux')
 
 // Stay backwards compatible with the single-chain-style config format
