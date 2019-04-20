f325c54e499154ac12aecd49b9d7de89bd07e6df
Angelo Laub <angelolaub@gmail.com>
2019-04-20 21:28:10 +0200

    remove wrong require

--- demux/index.js
+++ demux/index.js
@@ -3,7 +3,6 @@ const config = require('../common/config')
 const log = require('../common/log')
 const { fork } = require('child_process')
 const chains = require('../common/chains')
-const pm2 = require('pm2')
 const Demux = require('./demux')
 
 // Stay backwards compatible with the single-chain-style config format
